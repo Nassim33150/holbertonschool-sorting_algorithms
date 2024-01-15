5957d4f3f518d8a345198b6f289d0980a25c5383
root <root@LAPTOP-2LV4F0PM>
2024-01-15 22:26:31 +0100

    sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,17 +4,23 @@
 void bubble_sort(int *array, size_t size)
 {
     size_t i;
+    size_t j;
 
     size_t temp;
 
-    for (i = 0; i < size - 1; i++)
+    for (i = 0; i < size; i++)
     {
 
-        if (array[i] > array[i + 1])
+        for (j = 0; j < size - i - 1; j++)
+
         {
-            temp = array[i];
-            array[i] = array[i + 1];
-            array[i + 1] = temp;
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
         }
     }
 }
